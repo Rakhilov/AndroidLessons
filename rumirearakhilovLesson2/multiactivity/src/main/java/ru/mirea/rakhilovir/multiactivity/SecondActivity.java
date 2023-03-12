8978ee51884922acdef97778eee330dc87e24f01
Rakhilov <rakhilov@bk.ru>
2023-03-12 16:40:07 +0300

    Rakhilov lesson1

--- rumirearakhilovLesson2/multiactivity/src/main/java/ru/mirea/rakhilovir/multiactivity/SecondActivity.java
+++ rumirearakhilovLesson2/multiactivity/src/main/java/ru/mirea/rakhilovir/multiactivity/SecondActivity.java
@@ -7,6 +7,7 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
